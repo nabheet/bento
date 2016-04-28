5460639d2378d69263a619ea03b85689483ead9f
Nabheet S. Sandhu <nsandhu@godaddy.com>
2016-04-28 12:54:30 -0600

    reboot after update

--- scripts/common/update.sh
+++ scripts/common/update.sh
@@ -1,2 +1,4 @@
 yum install -y gcc libffi-devel openssl-devel mysql mysql-devel sqlite sqlite-devel libxml2 libxslt libxml2-devel libxslt-devel git python-devel libyaml-devel python-tools
 yum -y update
+reboot
+sleep 60
